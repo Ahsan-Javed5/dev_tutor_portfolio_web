925df62804870daa672a860357dc3bf5e74ef797
Ahsan <ahsanjaved5000@gmail.com>
2026-03-29 20:27:02 +0500

    push all local branch code to remote

---
+++ lib/core/utils/strings.dart
@@ -0,0 +1,18 @@
+class Strings {
+  static const titleDescription =
+      'I’m Ahsan Javed — Flutter Developer with Strong Mathematical Foundation & AI Focus';
+
+  static const aboutMeDescription =
+      'A Software Engineer and Mathematics Tutor with a strong academic background in Mathematics and Computer Science. I teach students internationally, helping them build strong foundations in Mathematics, Programming, and Problem-Solving with a clear and structured learning approach.Alongside teaching, I work as a professional Flutter Developer with over 4 years of experience building scalable mobile applications across fintech, transportation, telecom, and food-tech industries. I have developed and deployed real-world applications featuring secure payments, real-time tracking, and modern architecture used by thousands of users.My academic journey includes BSc in Mathematics & Computer Science, MSc Computer Science, and currently pursuing MS in Data Science. This combination allows me to bridge Mathematics, Programming, and Artificial Intelligence, while simplifying complex concepts for both students and clients.My goal is simple — to make complex concepts easy to understand, help students strengthen their fundamentals, and build intelligent, scalable applications that solve real-world problems.';
+
+  static const fullTitle =
+      'Flutter Developer | Mathematics Tutor | AI Enthusiast';
+
+  static const headerTitlePart1 = 'Flutter Developer ';
+
+  static const headerTitlePart2 = '| Mathematics Tutor';
+
+  static const headerTitlePart3 = ' | AI Enthusiast';
+
+  static const myName = 'AHSAN JAVED';
+}
